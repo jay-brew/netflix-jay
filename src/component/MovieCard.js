2295eefc574f05071c2cf3e4f8e9dc2945d06940
jay-brew <brew0@naver.com>
2022-09-15 11:37:31 +0900

    MovieCard detail

--- src/component/MovieCard.js
+++ src/component/MovieCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Badge } from 'react-bootstrap'
 
 const MovieCard = ({item}) => {
   console.log("item : ", item)
@@ -6,6 +7,15 @@ const MovieCard = ({item}) => {
     <div className='card' style={{
             backgroundImage:"url("+`https://www.themoviedb.org/t/p/w355_and_h200_multi_faces${item.poster_path}`+")"
         }}>
+          <div className='overlay'>
+            <h1>{item.title}</h1>
+            <div>
+              {item.genre_ids.map((id)=>(
+                <Badge bg="danger">id</Badge>
+              ))}
+            </div>
+            <div><span>{item.vote_average}</span><span>{item.adult ? "청불" : "Under 18"}</span></div>
+          </div>
     </div>
   )
 }
